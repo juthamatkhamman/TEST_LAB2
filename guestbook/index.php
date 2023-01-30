2d0c0cf352953e5413c929cfeeb1568168d9a2ab
juthamatkhamman <juthamat.kh@ku.th>
2023-01-30 10:06:48 +0700

    Add

--- guestbook/index.php
+++ guestbook/index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html>
-<body>
-    <form action="show.php">
-        what is your name:<input type="text" name ="name"><br>
-        what is your age:<input type="text"name="age"><br>
 
-        <input type="Submit"value="submit">
+<head>
+    <title>Personal Information</title>
+</head>
+
+<body>
+    <h1>Personal Information</h1>
+    <form action="show.php">
+        what is your name :<input type="text" name="name"><br>
+        what is your age :<input type="text" name="age"><br>
+        what is your gender (male/female):<input type="text" name="gender"><br>
+        what is your status (single/married):<input type="text" name="status"><br>
+        <input type="Submit" value="submit">
     </form>
 </body>
-
 </html>
--- guestbook/show.php
+++ guestbook/show.php
@@ -1,15 +1,56 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Personal Information</title>
+</head>
+
 <body>
+<h1>Personal Information</h1>
     <?php
+    
     $name = $_GET["name"];
     $age = $_GET["age"];
+    $gender = $_GET["gender"];
+    $status = $_GET["status"];
 
-    echo "Name:"."$name"."<br>";
-    echo "Age:"."$age"."<br>";
+    echo "Name : ";
+    if ($age <= "14") #อายุน้อยกว่าหรือเท่ากับ 14 ปี
+    { if ($gender == "male") 
+        {
+            echo "Mstr."; #เด็กชาย
+        }
+        else if ($gender == "female")
+        {
+           echo "Miss"; #เด็กหญิง
+        }
+    } 
+    else if ($age >= "15") //อายุมากกว่าหรือเท่ากับ 15 ปี
+    { if ($gender == "male")
+        {
+            echo "Mr."; //นาย
+        }
+        else if ($gender == "female")
+        { if ($status == "married") //สถานะแต่งงานแล้ว
+            {
+                echo "Mrs."; //นาง
+            }
+            else if ($status == "single") //สถานะโสด
+            {
+                echo "Miss"; //นางสาว
+            }
+            
+        }
+    }
+    
+    
+    echo "  " . "$name" . "<br>";
+    echo "Age : " . "$age" . "<br>";
+    echo "Gender : " . "$gender" . "<br>";
+    echo "Status : " . "$status" . "<br>";
+
     ?>
 
-    
+
 </body>
 
 </html>
