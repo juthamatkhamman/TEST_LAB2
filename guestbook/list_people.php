077a05aabdffb5bb503d479c2e16617ec5cdb671
juthamatkhamman <juthamat.kh@ku.th>
2023-01-30 10:20:04 +0700

    M

--- guestbook/list_people.php
+++ guestbook/list_people.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "cpe0597";
+$password = "@@##&&Pj240502";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
